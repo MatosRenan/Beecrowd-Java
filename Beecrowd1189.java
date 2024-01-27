d407f7c011fd981e43fdf7d47761f92fee1b676b
matosrenan <98982899+MatosRenan@users.noreply.github.com>
2024-01-27 14:59:32 -0300

    Add files via upload

---
+++ Beecrowd1189.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Beecrowd1189 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<ArrayList<Double>> matrix = new ArrayList<>();
+        String x = sc.next();
+        double sum = 0;
+
+        for (int i = 0; i < 12; i++) {
+            ArrayList<Double> line = new ArrayList<>();
+
+            for (int j = 0; j < 12; j++) {
+                line.add(sc.nextDouble());
+            }
+            matrix.add(line);
+        }
+
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
+
+                if (i > j && i + j < 11) {
+                    sum += matrix.get(i).get(j);
+                }
+            }
+        }
+
+        if (x.equals("S")) {
+            System.out.printf("%.1f%n", sum);
+        } else if (x.equals("M")) {
+            System.out.printf("%.1f%n", sum/30);
+        }
+    }
+}

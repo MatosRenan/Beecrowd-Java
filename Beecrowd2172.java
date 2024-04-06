89092bb5a3025fdb4752f7aaa2db609bc98246b3
Renan <98982899+MatosRenan@users.noreply.github.com>
2024-04-06 17:37:03 -0300

    Add files via upload

---
+++ Beecrowd2172.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Beecrowd2172 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int m = sc.nextInt();
+
+        while (x != 0 && m != 0) {
+            System.out.println(x * m);
+
+            x = sc.nextInt();
+            m = sc.nextInt();
+        }
+
+    }
+}

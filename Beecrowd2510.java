5743122a26a338da809ca1c0e5c3cb0dbb9fd97b
Renan <98982899+MatosRenan@users.noreply.github.com>
2024-06-03 18:52:20 -0300

    Add files via upload

---
+++ Beecrowd2510.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Beecrowd2510 {
+    public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            int n = sc.nextInt();
+
+            for (int i = 0; i < n; i++) {
+
+                String villain = sc.next();
+                System.out.println("Y");
+            }
+    }
+}
